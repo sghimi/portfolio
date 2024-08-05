b45856002e639fdcba8fa997060689d0fac1c554
Saugat Ghimire <sgdoger@gmail.com>
2024-08-05 07:51:57 -0500

    Additional updates + initial dark mode implementation

---
+++ src/ThemeContext.js
@@ -0,0 +1,28 @@
+// ThemeContext.js
+import React, { createContext, useState, useEffect } from "react";
+
+export const ThemeContext = createContext();
+
+const ThemeProvider = ({ children }) => {
+  const [theme, setTheme] = useState("light");
+
+  // Load theme from local storage or default to 'light'
+  useEffect(() => {
+    const savedTheme = localStorage.getItem("theme") || "light";
+    setTheme(savedTheme);
+  }, []);
+
+  const toggleTheme = () => {
+    const newTheme = theme === "light" ? "dark" : "light";
+    setTheme(newTheme);
+    localStorage.setItem("theme", newTheme);
+  };
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+export default ThemeProvider;
